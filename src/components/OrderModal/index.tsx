b41b2c10f22ccc4140da735b2b931f5cb614d650
tonymatheus <tony.chaves14@gmail.com>
2022-12-20 23:16:57 -0300

    chore:📝 Implement Modal

---
+++ src/components/OrderModal/index.tsx
@@ -0,0 +1,96 @@
+import { ModalBody, Overlay, OrderDetails, Actions } from './styles';
+import CloseIcon from '../../assets/images/close-icon.svg';
+import { Order } from '../../types/Order';
+import { formatCurrancy } from '../../utils/formatCurrancy';
+import { useEffect } from 'react';
+
+interface OrderModalProps {
+  visible: boolean;
+  order: Order | null;
+  onClose: () => void;
+}
+export const OrderModal = ({ visible, order, onClose }: OrderModalProps) => {
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
+  if (!visible || !order) {
+    return null;
+  }
+
+  const total = order.products.reduce((accumulator, { quantity, product }) => {
+    return accumulator + product.price * quantity;
+  }, 0);
+
+  return (
+    <Overlay>
+      <ModalBody>
+        <header>
+          <strong>Mesa {order.table}</strong>
+          <button type="button" onClick={onClose}>
+            <img src={CloseIcon} alt="[icone de fechar" />
+          </button>
+        </header>
+
+        <div className="status-container">
+          <small>Status do Pedido</small>
+          <div>
+            <span>{order.status === 'WAITING' && '⏱️'}</span>
+            <span>{order.status === 'IN_PRODUCTION' && '👨🏾‍🍳'}</span>
+            <span>{order.status === 'DONE' && '✅'}</span>
+            <strong>
+              <span>{order.status === 'WAITING' && 'Fila de Espera'}</span>
+              <span>{order.status === 'IN_PRODUCTION' && 'Em Produção'}</span>
+              <span>{order.status === 'DONE' && 'Pronto'}</span>
+            </strong>
+          </div>
+        </div>
+        <OrderDetails>
+          <strong>Itens</strong>
+
+          <div className="order-items">
+            {order.products.map(({ _id, quantity, product }) => (
+              <div className="item" key={_id}>
+                <img
+                  width="48"
+                  height="28.51"
+                  src={`http://localhost:3001/uploads/${product.imagePath}`}
+                  alt={product.name}
+                />
+
+                <span className="quantity">{quantity}X</span>
+                <div className="product-details">
+                  <strong>{product.name}</strong>
+                  <span>{formatCurrancy(product.price)}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="total">
+            <span>Total</span>
+            <strong>{formatCurrancy(total)}</strong>
+          </div>
+          <Actions>
+            <button type="button" className="primary">
+              <span>👨🏾‍🍳</span>
+              <span>Iniciar Produção</span>
+            </button>
+            <button type="button" className="secondary">
+              <span>cancelar pedido</span>
+            </button>
+          </Actions>
+        </OrderDetails>
+      </ModalBody>
+    </Overlay>
+  );
+};
