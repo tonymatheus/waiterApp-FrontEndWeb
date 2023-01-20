127e5d41c426b7fcd75190ee9cbddf282d7e8c5a
tonymatheus <tony.chaves14@gmail.com>
2023-01-19 22:52:01 -0300

    Chore:📝 remove unused state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import { useState } from 'react';
 import { GlobalStyles } from './styles/GlobalStyles';
 
 import { Header } from './components/Header';
 import { Orders } from './components/Orderds';
 
 function App() {
-
   return (
     <>
       <GlobalStyles />
